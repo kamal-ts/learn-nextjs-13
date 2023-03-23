8c115da682d20ef5b9685e08a08201004e9f0781
kamal1234567890 <kamaluddin1623@gmail.com>
2023-03-24 04:36:56 +0900

    completed add todo

--- app/todos/addTodos.tsx
+++ app/todos/addTodos.tsx
@@ -1,11 +1,43 @@
 "use client";
 
-import { useState } from "react";
+import { useState, SyntheticEvent } from "react";
+import { useRouter } from "next/navigation";
 
 function AddTodos() {
   const [modal, setModal] = useState(false);
+  const [title, setTitle] = useState("");
+  const [completed, setCompleted] = useState("");
+
+  const router = useRouter();
   function handleChange() {
     setModal(!modal);
+  }
+
+  function handleClose() {
+    setTitle("");
+    setCompleted("");
+    setModal(false);
+  }
+
+  async function createTodos(e: SyntheticEvent) {
+    e.preventDefault();
+
+    const isCompleted = completed === "true" ? true : false;
+
+    await fetch("http://localhost:8000/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        todo: title,
+        isCompleted: isCompleted,
+      }),
+    });
+    setTitle("");
+    setCompleted("");
+    router.refresh();
+    setModal(false);
   }
 
   return (
@@ -24,31 +56,38 @@ function AddTodos() {
       <div className="modal ">
         <div className="modal-box">
           <h3 className="font-bold text-lg ">Add New Todos</h3>
-          <form action="">
+          <form onSubmit={createTodos}>
             <div className="form-control">
               <label htmlFor="" className="label font-bold">
                 Todos
               </label>
               <input
+                value={title}
                 type="text"
                 className="input w-full input-bordered"
                 placeholder="Todos"
+                onChange={(e) => setTitle(e.target.value)}
               />
             </div>
             <div className="form-control">
               <label htmlFor="" className="label font-bold">
                 Completed
               </label>
-              <select className="select select-bordered w-full">
-                <option disabled selected>
+              <select
+                value={completed}
+                defaultValue={""}
+                onChange={(e) => setCompleted(e.target.value)}
+                className="select select-bordered w-full"
+              >
+                <option value={""} disabled selected>
                   Is completed?
                 </option>
-                <option value={1}>yes</option>
-                <option value={0}>no</option>
+                <option value={"true"}>yes</option>
+                <option value={"false"}>no</option>
               </select>
             </div>
             <div className="modal-action">
-              <button className="btn" type="button" onClick={handleChange}>
+              <button className="btn" type="button" onClick={handleClose}>
                 Close
               </button>
               <button className="btn btn-primary" type="submit">
